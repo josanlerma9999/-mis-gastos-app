378f6cbb8f2797d45905e8490161a38e6845e93d
josanlerma9999 <josanlerma@gmail.com>
2026-04-10 20:31:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE_NAME = 'bbva-v1';
-const assets = ['./', './index.html'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
